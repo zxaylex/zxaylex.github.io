dbdfa87f9a513c57d73f97ce3ca2c8a3d7ccb3f8
Zach David <112095798+zxaylex@users.noreply.github.com>
2025-07-11 10:55:56 +0800

    added some states for later

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,28 @@
+
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Tab from './components/Tab';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [links, setLinks] = useState(0);
+  const [about, setAbout] = useState(0);
+  const [professional, setProfessional] = useState(0);
+  const [team, setTeam] = useState(0);
+  const [contact, setContact] = useState(0);
+  const [activeTab, setActiveTab] = useState('home');
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
+      <div className="browser-container">
+      <div className="tab-container">
+        <Tab name='Home'/>
+      </div>
+        <p className='title-name'>Hi! I am <strong>Zach David</strong></p>
+        <p className='title-intro'>Your friendly software developer and enthusiast.</p>
+      </div>
+      <div className="tab-selection">
         
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
