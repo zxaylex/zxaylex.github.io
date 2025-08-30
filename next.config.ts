f70cc60a85fbf5ae23a0622cb7436db7ddd802bd
Zach <112095798+zxaylex@users.noreply.github.com>
2025-08-30 15:44:41 +0000

    minor changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
+
 const nextConfig: NextConfig = {
   /* config options here */
+  skipMiddlewareUrlNormalize: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  output: "export"
 };
 
 export default nextConfig;
