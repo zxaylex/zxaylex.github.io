cca7a3afe19f7ce6c6e2dc488a058255fa4a597f
Zach <112095798+zxaylex@users.noreply.github.com>
2025-08-30 15:55:03 +0000

    changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,7 +18,7 @@ const eslintConfig = [
       "out/**",
       "build/**",
       "next-env.d.ts",
-    ],
+    ]
   },
 ];
 
--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   skipMiddlewareUrlNormalize: true,
+  trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
